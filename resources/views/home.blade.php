c17a2447f353ca29036f07233620b0939c73ba46
daugaard47 <daugaard47@gmail.com>
2020-06-30 01:40:40 -0400

    Quick text fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,8 @@
                 </div>
             @endif
             <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
-                <div class="font-semibold bg-gray-200 text-gray-700 py-4 px-6 mb-0">
-                    Dashboard
+                <div class="font-semibold bg-gray-200 text-gray-700 py-4 px-6 mb-0 capitalize">
+                    Add a post
                 </div>
                 <livewire:post-index/>
             </div>
