08256df09edd22bf7c9464d487cf8df22b2eaf8a
daugaard47 <daugaard47@gmail.com>
2020-06-30 01:43:30 -0400

    Resonsive Fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="flex items-center">
-        <div class="md:w-1/2 md:mx-auto">
+        <div class="md:w-1/2 md:px-0 mx-auto w-full px-6">
             @if (session('status'))
                 <div
                     class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4"
